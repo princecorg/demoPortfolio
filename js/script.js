eae2e1c55b6dd073e35f0809a6d02930b0f1ffea
Christophe Vallot <cvallot76@gmail.com>
2022-12-01 15:50:06 +0100

    Ajout case à cocher consentement RGPD

--- js/script.js
+++ js/script.js
@@ -1,40 +1,55 @@
 /* Ajout ou suppression de la classe "responsive" au clic */
 function toogleResponsive() {
-    var x = document.getElementById("myTopnav");
-    if (x.className === "topnav") {
-      x.className += " responsive";
-    } else {
-      x.className = "topnav";
-    }
+  var x = document.getElementById("myTopnav");
+  if (x.className === "topnav") {
+    x.className += " responsive";
+  } else {
+    x.className = "topnav";
   }
+}
 
 /* Gestion  du diaporama */
 
-  let slideIndex = 1;
+let slideIndex = 1;
 showSlides(slideIndex);
 
 // Next/previous controls
 function plusSlides(n) {
-  showSlides(slideIndex += n);
+  showSlides((slideIndex += n));
 }
 
 // Thumbnail image controls
 function currentSlide(n) {
-  showSlides(slideIndex = n);
+  showSlides((slideIndex = n));
 }
 
 function showSlides(n) {
   let i;
   let slides = document.getElementsByClassName("mySlides");
   let dots = document.getElementsByClassName("dot");
-  if (n > slides.length) {slideIndex = 1}
-  if (n < 1) {slideIndex = slides.length}
+  if (n > slides.length) {
+    slideIndex = 1;
+  }
+  if (n < 1) {
+    slideIndex = slides.length;
+  }
   for (i = 0; i < slides.length; i++) {
     slides[i].style.display = "none";
   }
   for (i = 0; i < dots.length; i++) {
     dots[i].className = dots[i].className.replace(" active", "");
   }
-  slides[slideIndex-1].style.display = "block";
-  dots[slideIndex-1].className += " active";
+  slides[slideIndex - 1].style.display = "block";
+  dots[slideIndex - 1].className += " active";
+}
+
+function controleForm() {
+  if (document.getElementById("rgpd").checked == false) {
+    alert(
+      "Vous devez donner votre consentement avant de soumettre le formulaire"
+    );
+  } else {
+    alert("Le formulaire a bien été envoyé");
+    submit();
+  }
 }
